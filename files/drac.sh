c146878f16ff362c875d527919db70ce6627d180
Tim Short <tim.short@cabernet.tech>
2019-10-25 12:05:31 +0100

    added doit mode with command echo in soft mode

--- files/drac.sh
+++ files/drac.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
+# Docker Remove All Containers
+
+app='echo'
+while getopts x arg; do
+	case $arg in
+		x) app='';;
+		*) echo 'Bad arg - bye'; exit 42;;
+	esac
+done
+
 victims=$(sudo docker ps -aq)
 for victim in $victims; do
-	echo "removing $victim"
-	sudo docker rm $victim
+	$app sudo docker rm $victim
 done
 exit 0
