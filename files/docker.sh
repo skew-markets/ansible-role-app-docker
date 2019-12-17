04fc7635dd1cceab933a84606bbdb55cd64b1e55
Tim Short <tim.short@cabernet.tech>
2019-12-17 13:37:29 +0000

    added dpsx for listing exited containers

--- files/docker.sh
+++ files/docker.sh
@@ -1,3 +1,4 @@
+# MANAGED_BY_ANSIBLE
 # --------------------------------
 
 alias   dock='sudo docker'
@@ -5,11 +6,12 @@ alias  dimgs='dock images'
 alias  dlogs='dock logs'
 alias    dps='dock ps'
 alias   dpsa='dock ps -a'
+alias   dpsx='dock ps --filter status=exited'
 alias  dpsai='dock ps -aq | tr "\n" " " '
+
 alias dshell='dock run -i -t --rm'
 alias   dvol='dock volume ls'
 
 alias vid='vi Dockerfile'
 
 # --------------------------------
-# MANAGED_BY_ANSIBLE
