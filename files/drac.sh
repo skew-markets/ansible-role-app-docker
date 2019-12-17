5a7704795b434d1f2d70650a0b64911307dd88a2
Tim Short <tim.short@cabernet.tech>
2019-12-17 13:37:04 +0000

    defaults to only removing exited containers

--- files/drac.sh
+++ files/drac.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-# Docker Remove All Containers
+# Removes Docker containers
+#
+# Defaults
+# 	just removes containers that have exited
+#   echos the 'docker rm' command line that it would have run
+# Options
+# 	-a means all containers
+#   -x runs the 'docker rm' command
+# --------------------------------
 
+args="--filter status=exited"
 app='echo'
-while getopts x arg; do
+while getopts ax arg; do
 	case $arg in
+		a) args="-a";;
 		x) app='';;
 		*) echo 'Bad arg - bye'; exit 42;;
 	esac
 done
 
-victims=$(sudo docker ps -aq)
+victims=$(sudo docker ps -q $args)
 for victim in $victims; do
 	$app sudo docker rm $victim
 done
+
+# --------------------------------
 exit 0
